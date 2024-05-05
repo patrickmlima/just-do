64bba61b838cc3733e2f5d9bd491658e91f18cf7
Patrick Lima <patrickm.lima1@gmail.com>
2024-05-05 18:43:06 -0400

    feat: Import config and db on app module

--- just-do-backend/src/app.module.ts
+++ just-do-backend/src/app.module.ts
@@ -1,10 +1,27 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
+
+import { ApiModule } from './api/api.module';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { ApiModule } from './api/api.module';
+import { AuthModule } from './auth/auth.module';
+import appConfig from './config/app.config';
+import dbConfig from './config/db.config';
+import { DatabaseModule } from './database/database.module';
+
+const nodeEnv = process.env.NODE_ENV;
 
 @Module({
-  imports: [ApiModule],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+      envFilePath: `.env.${nodeEnv ?? ''}`,
+      load: [appConfig, dbConfig],
+    }),
+    ApiModule,
+    AuthModule,
+    DatabaseModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
