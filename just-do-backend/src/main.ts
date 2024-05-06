f017a0bc208efa413282d345e2b775da1cf75d46
Patrick Lima <patrickm.lima1@gmail.com>
2024-05-06 00:35:10 -0400

    feat: Add swagger configuration

--- just-do-backend/src/main.ts
+++ just-do-backend/src/main.ts
@@ -1,10 +1,27 @@
+import { INestApplication } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
-import { AppModule } from './app.module';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+
 import { ConfigService } from '@nestjs/config';
+import { AppModule } from './app.module';
 import { AppConfig } from './config/app.config';
+
+const bootstrapSwagger = (app: INestApplication) => {
+  const config = new DocumentBuilder()
+    .setTitle('Just Do API')
+    .setDescription('The straight and simple To Do app')
+    .setVersion('1.0')
+    .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api/docs', app, document);
+};
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  bootstrapSwagger(app);
+
   const configService = app.get(ConfigService);
   const appConfig = configService.get<AppConfig>('application');
   await app.listen(appConfig.appPort);
