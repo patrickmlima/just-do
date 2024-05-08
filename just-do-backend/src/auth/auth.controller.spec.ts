97e99e7a350d1cabae2d170975624090bcb63d79
Patrick Lima <patrickm.lima1@gmail.com>
2024-05-08 16:13:10 -0400

    test: Config auth controller tests

--- just-do-backend/src/auth/auth.controller.spec.ts
+++ just-do-backend/src/auth/auth.controller.spec.ts
@@ -1,12 +1,30 @@
+import { JwtModule } from '@nestjs/jwt';
 import { Test, TestingModule } from '@nestjs/testing';
+import { getRepositoryToken } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+
+import { PasswordService } from 'src/api/users/password/password.service';
+import { UsersService } from 'src/api/users/users.service';
+import { User } from 'src/database/entities/user.entity';
 import { AuthController } from './auth.controller';
+import { AuthService } from './auth.service';
 
 describe('AuthController', () => {
   let controller: AuthController;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
+      imports: [JwtModule.register({ secret: 'justas3cr3tf0rt3sts' })],
       controllers: [AuthController],
+      providers: [
+        AuthService,
+        UsersService,
+        PasswordService,
+        {
+          provide: getRepositoryToken(User),
+          useClass: Repository,
+        },
+      ],
     }).compile();
 
     controller = module.get<AuthController>(AuthController);
