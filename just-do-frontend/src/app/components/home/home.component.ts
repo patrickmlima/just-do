5f1d8a82b89bc2defe2941d2867a2e882871d938
Patrick Lima <patrickm.lima1@gmail.com>
2024-05-10 12:38:11 -0400

    [fe] feat: Add header and list tasks component

--- just-do-frontend/src/app/components/home/home.component.ts
+++ just-do-frontend/src/app/components/home/home.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
+import { HeaderComponent } from '../header/header.component';
 import { TasksListComponent } from '../tasks-list/tasks-list.component';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [TasksListComponent],
+  imports: [HeaderComponent, TasksListComponent],
   templateUrl: './home.component.html',
   styleUrl: './home.component.sass',
 })
