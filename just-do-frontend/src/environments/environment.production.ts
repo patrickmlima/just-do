033948f46653e4d5e825cdc6048c3afcc3115392
Patrick Lima <patrickm.lima1@gmail.com>
2024-05-10 21:21:07 -0400

    [fe|be] chore: Add docker files to run services

---
+++ just-do-frontend/src/environments/environment.production.ts
@@ -0,0 +1,14 @@
+export const environment = {
+  authServer: {
+    protocol: 'http',
+    host: 'backend',
+    port: '3000',
+    uri: 'auth',
+  },
+  mainServer: {
+    protocol: 'http',
+    host: 'backend',
+    port: '3000',
+    uri: 'api',
+  },
+};
