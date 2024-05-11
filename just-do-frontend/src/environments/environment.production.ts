971318ced53b27fb752ae16ad92b3d4197368754
Patrick Lima <patrickm.lima1@gmail.com>
2024-05-10 22:19:53 -0400

    fix: Server hosts for backend

--- just-do-frontend/src/environments/environment.production.ts
+++ just-do-frontend/src/environments/environment.production.ts
@@ -1,13 +1,13 @@
 export const environment = {
   authServer: {
     protocol: 'http',
-    host: 'backend',
+    host: 'localhost',
     port: '3000',
     uri: 'auth',
   },
   mainServer: {
     protocol: 'http',
-    host: 'backend',
+    host: 'localhost',
     port: '3000',
     uri: 'api',
   },
